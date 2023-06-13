0b1ddd448d64811e6c3367075eed69a418b90b20
fabyr <fabyr@proton.me>
2023-06-13 14:13:28 +0200

    Adjust WindowsInput (No delay, both modes)

--- input_windows.py
+++ input_windows.py
@@ -2,11 +2,26 @@ from input import Input
 import pyautogui
 import pydirectinput
 
-# This code is functional on windows, though terribly slow
 class InputWindows(Input):
+    direct: bool = False
+
+    def __init__(self) -> None:
+        super().__init__()
+        pyautogui.PAUSE = 0
+        pydirectinput.PAUSE = 0
+
     def mouse_up(self) -> None:
-        pydirectinput.mouseUp()
+        if self.direct:
+            pydirectinput.mouseUp()
+        else:
+            pyautogui.mouseUp()
     def mouse_down(self) -> None:
-        pydirectinput.mouseDown()
+        if self.direct:
+            pydirectinput.mouseDown()
+        else:
+            pyautogui.mouseDown()
     def mouse_move_relative(self, dx : int, dy : int) -> None:
-        pydirectinput.moveRel(dx, dy, relative=True)
+        if self.direct:
+            pydirectinput.moveRel(dx, dy)
+        else:
+            pyautogui.moveRel(dx, dy)
