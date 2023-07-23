433a4a6cc8aeff116af2072e49fe54b1f6c4d994
fabyr <fabyr@proton.me>
2023-07-23 02:35:27 +0200

    GUI Overhaul - Switch to CustomTkInter for a fancier GUI - Scrollable Frame - Fix bug where first column changes spacing if image is too large

--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
-from tkinter import *
-from tkinter import filedialog
+import customtkinter as ctk
 
 from pynput import keyboard # for hotkeys
 from PIL import ImageTk, Image, ImageGrab
@@ -15,20 +14,21 @@ import time
 import threading
 
 class CursorImagerGUI:
-    def numberValidation(self, s) -> None:
-        if s in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-            return True
-        self.window.bell() # bell() plays sound to signal wrong input
-        return False
+    def number_validation(self, s) -> None:
+        s.set("".join(filter(lambda c: c if c in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] else '', s.get())))
 
-    def __init__(self) -> None:
-        self.window = Tk()
+    def __init__(self, width=750, height=850) -> None:
+        self.root = ctk.CTk()
+        self.root_frame = ctk.CTkFrame(self.root)
+        self.root_frame.pack(fill="both", expand=True)
+        self.window = ctk.CTkScrollableFrame(self.root_frame)
+        self.window.pack(fill="both", expand=True)
         self.current_hotkey = None
         self.hotkey_listener = None
         self.bg = None
         self.last_press = time.time()
-        self.window.title("Cursor Imager")
-        self.window.geometry("800x800")
+        self.root.title("Cursor Imager")
+        self.root.geometry("%ix%i" % (width, height))
         
         self.input = None
         self.is_linux = False
@@ -49,93 +49,105 @@ class CursorImagerGUI:
         ### GUI Setup ###
         #################
         self.window.columnconfigure(1, weight=0)
-        self.window.configure(padx=30, pady=30)
+        self.root.configure(padx=18, pady=18)
 
-        self.btnSelectImage = Button(self.window, text="Select Image", fg="black", command=self.button_select_image)
+        self.btnSelectImage = ctk.CTkButton(self.window, text="Select Image", command=self.button_select_image)
         self.btnSelectImage.grid(sticky="WE")
-        self.btnFromClipboard = Button(self.window, text="Load from Clipboard", fg="black", command=self.button_select_image_clipboard)
+        self.btnFromClipboard = ctk.CTkButton(self.window, text="Load from Clipboard", command=self.button_select_image_clipboard)
         self.btnFromClipboard.grid(sticky="WE")
-        self.lblHotkey = Label(self.window, text="Hotkey: ")
+        self.lblHotkey = ctk.CTkLabel(self.window, text="Hotkey: ")
         self.lblHotkey.grid(sticky="N")
-        self.txtSetHotkey = Entry(self.window, text="<None>", fg="black")
+        self.txtSetHotkey = ctk.CTkEntry(self.window)
+        self.txtSetHotkey.insert(ctk.END, "<None>")
         self.txtSetHotkey.bind("<Key>", self.txt_hotkey_type)
         self.txtSetHotkey.grid(sticky="N")
-        self.btnReset = Button(self.window, text="Reset Drawing Progress", fg="black", command=self.button_reset)
+        self.btnReset = ctk.CTkButton(self.window, text="Reset Drawing Progress", command=self.button_reset)
         self.btnReset.grid(sticky="WE")
 
-        self.lblSpeedInfo = Label(self.window, text="Drawing Speed: ")
-        self.lblSpeedInfo.grid(sticky="N", pady=(15, 0))
-        self.scaleSpeedValue = DoubleVar()
-        self.scaleSpeed = Scale(self.window, from_=1, to=20, orient='horizontal', variable=self.scaleSpeedValue, command=self.scale_speed_slide)
+        self.lblSpeedInfo = ctk.CTkLabel(self.window, text="Drawing Speed: ")
+        self.lblSpeedInfo.grid(sticky="N")
+        self.scaleSpeedValue = ctk.DoubleVar()
+        self.scaleSpeedValue.set(1)
+        self.scaleSpeed = ctk.CTkSlider(self.window, from_=1, to=20, orientation='horizontal', variable=self.scaleSpeedValue, command=self.scale_speed_slide)
         self.scaleSpeed.grid(sticky="WE")
 
-        self.lblScaleInfo = Label(self.window, text="Drawing Scale: ")
+        self.lblScaleInfo = ctk.CTkLabel(self.window, text="Drawing Scale: ")
         self.lblScaleInfo.grid(sticky="N")
-        self.scaleScaleValue = DoubleVar()
-        self.scaleScale = Scale(self.window, from_=1, to=10, orient='horizontal', variable=self.scaleScaleValue, command=self.scale_scale_slide)
-        self.scaleScale.grid(sticky="WE", pady=(0, 0))
+        self.scaleScaleValue = ctk.DoubleVar()
+        self.scaleScaleValue.set(1)
+        self.scaleScale = ctk.CTkSlider(self.window, from_=1, to=10, orientation='horizontal', variable=self.scaleScaleValue, command=self.scale_scale_slide)
+        self.scaleScale.grid(sticky="WE")
 
-        self.lblDecimationInfo = Label(self.window, text="Point Decimation: ")
-        self.lblDecimationInfo.grid(sticky="N")
-        self.scaleDecimationValue = DoubleVar()
-        self.scaleDecimation = Scale(self.window, from_=1, to=25, orient='horizontal', variable=self.scaleDecimationValue, command=self.scale_decimation_slide)
-        self.scaleDecimation.grid(sticky="WE", pady=(0, 0))
+        self.lblDecimationInfo = ctk.CTkLabel(self.window, text="Point Decimation: ")
+        self.lblDecimationInfo.grid(sticky="N",)
+        self.scaleDecimationValue = ctk.DoubleVar()
+        self.scaleDecimationValue.set(1)
+        self.scaleDecimation = ctk.CTkSlider(self.window, from_=1, to=25, orientation='horizontal', variable=self.scaleDecimationValue, command=self.scale_decimation_slide)
+        self.scaleDecimation.grid(sticky="WE")
         
-        self.cbDoEdgeValue = BooleanVar()
-        self.cbDoEdge = Checkbutton(self.window, text="Edge detection", variable=self.cbDoEdgeValue, onvalue=True, offvalue=False, command=self.cbDoEdge_press)
+        self.cbDoEdgeValue = ctk.BooleanVar()
+        self.cbDoEdge = ctk.CTkCheckBox(self.window, text="Edge detection", variable=self.cbDoEdgeValue, onvalue=True, offvalue=False, command=self.cbDoEdge_press)
         self.cbDoEdge.grid(sticky="NW")
 
-        self.cbInvertValue = BooleanVar()
-        self.cbInvert = Checkbutton(self.window, text="Invert", variable=self.cbInvertValue, onvalue=True, offvalue=False, command=self.cbInvert_press)
+        self.cbInvertValue = ctk.BooleanVar()
+        self.cbInvert = ctk.CTkCheckBox(self.window, text="Invert", variable=self.cbInvertValue, onvalue=True, offvalue=False, command=self.cbInvert_press)
         self.cbInvert.grid(sticky="NW")
 
-        self.cbRDPValue = BooleanVar()
-        self.cbRDP = Checkbutton(self.window, text="Polyline Simplification", variable=self.cbRDPValue, onvalue=True, offvalue=False, command=self.cbRDP_press)
+        self.cbRDPValue = ctk.BooleanVar()
+        self.cbRDP = ctk.CTkCheckBox(self.window, text="Polyline Simplification", variable=self.cbRDPValue, onvalue=True, offvalue=False, command=self.cbRDP_press)
         self.cbRDP.grid(sticky="NW")
 
-        self.cbDirectInputValue = BooleanVar()
-        self.cbDirectInput = Checkbutton(self.window, text="Use DirectInput", variable=self.cbDirectInputValue, state='disabled' if self.is_linux else 'normal', onvalue=True, offvalue=False, command=self.cbDirectInput_press)
+        self.cbDirectInputValue = ctk.BooleanVar()
+        self.cbDirectInput = ctk.CTkCheckBox(self.window, text="Use DirectInput", variable=self.cbDirectInputValue, state='disabled' if self.is_linux else 'normal', onvalue=True, offvalue=False, command=self.cbDirectInput_press)
         self.cbDirectInput.grid(sticky="NW")
 
-        self.lblMaxApartInfo = Label(self.window, text="Max Path Combine Distance: ")
+        self.lblMaxApartInfo = ctk.CTkLabel(self.window, text="Max Path Combine Distance: ")
         self.lblMaxApartInfo.grid(sticky="N")
         
-        vcmd = (self.window.register(self.numberValidation), '%S')
-
-        self.txtMaxApartValue = StringVar()
-        self.txtMaxApart = Entry(self.window, validate="key", vcmd=vcmd, textvariable=self.txtMaxApartValue)
+        self.txtMaxApartValue = ctk.StringVar()
+        self.txtMaxApartValue.trace("w", lambda name, index, mode, sv=self.txtMaxApartValue: self.number_validation(sv))
+        self.txtMaxApart = ctk.CTkEntry(self.window, textvariable=self.txtMaxApartValue)
         self.txtMaxApart.bind("<KeyRelease>", self.txt_max_apart)
         self.txtMaxApart.grid(sticky="WE")
         self.txtMaxApartValue.set("3")
 
-        self.lblMaxContinousInfo = Label(self.window, text="Max Continous Path: ")
+        self.lblMaxContinousInfo = ctk.CTkLabel(self.window, text="Max Continous Path: ")
         self.lblMaxContinousInfo.grid(sticky="N")
         
-        self.txtMaxContinousValue = StringVar()
-        self.txtMaxContinous = Entry(self.window, validate="key", vcmd=vcmd, textvariable=self.txtMaxContinousValue)
+        self.txtMaxContinousValue = ctk.StringVar()
+        self.txtMaxContinousValue.trace("w", lambda name, index, mode, sv=self.txtMaxContinousValue: self.number_validation(sv))
+        self.txtMaxContinous = ctk.CTkEntry(self.window, textvariable=self.txtMaxContinousValue)
         self.txtMaxContinous.bind("<KeyRelease>", self.txt_max_continous)
         self.txtMaxContinous.grid(sticky="WE")
         self.txtMaxContinousValue.set("10000")
 
-        self.btnRecalc = Button(self.window, text="(Re)Calculate Image", fg="black", command=self.recalc_image)
+        self.btnRecalc = ctk.CTkButton(self.window, text="(Re)Calculate Image", command=self.recalc_image)
         self.btnRecalc.grid(sticky="WE")
-
-        self.lblInfo = Label(self.window, text="Preview image (White pixels will be drawn)")
-        self.lblInfo.grid(sticky="NW", row=0, column=1, padx=20)
-
-        self.imgDisplay = Label(self.window, borderwidth=1, relief="solid")
-        self.imgDisplay.grid(sticky="NW", row=1, rowspan=16, column=1, padx=20, pady=20)
 
         self.lblInitialText = "Not drawing."
         self.lblDrawingText = "Drawing in progress..."
-        self.lblDrawingStatus = Label(self.window, text=self.lblInitialText)
-        self.lblDrawingStatus.grid(sticky="N", padx=20, pady=5)
+        self.lblDrawingStatus = ctk.CTkLabel(self.window, text=self.lblInitialText)
+        self.lblDrawingStatus.grid(sticky="N")
 
-        self.lblPointCount = Label(self.window, text="Point count: 0")
-        self.lblPointCount.grid(sticky="N", padx=20, pady=5)
+        self.lblPointCount = ctk.CTkLabel(self.window, text="Point count: 0")
+        self.lblPointCount.grid(sticky="N")
         
-        self.lblDrawingPercentage = Label(self.window, text="0.0% Done")
-        self.lblDrawingPercentage.grid(sticky="N", padx=20, pady=5)
+        self.lblDrawingPercentage = ctk.CTkLabel(self.window, text="0.0% Done")
+        self.lblDrawingPercentage.grid(sticky="N")
+
+        self.lblInfo = ctk.CTkLabel(self.window, text="Preview image (White pixels will be drawn)")
+        self.lblInfo.grid(sticky="NW", row=0, column=1, padx=(20, 0))
+
+        self.imgDisplay = ctk.CTkLabel(self.window, text="")
+        #self.imgDisplay.grid(sticky="NW", row=1, rowspan=16, column=1, padx=(20, 0))
+        self.imgDisplay.grid(sticky="NW", row=1, rowspan=1024, column=1, padx=(20, 0))
+
+        col_count, row_count = self.window.grid_size()
+        for col in range(col_count):
+            self.window.grid_columnconfigure(col, pad=8)
+
+        for row in range(row_count):
+            self.window.grid_rowconfigure(row, pad=8)
 
         # initialize core drawing functionality
         self.params = core.DrawingParameters()
@@ -145,6 +157,7 @@ class CursorImagerGUI:
         # and coherent with the GUI
         self.scale_speed_slide(None)
         self.scale_scale_slide(None)
+        self.scale_decimation_slide(None)
         self.cbDoEdge_press()
         self.cbInvert_press()
         self.txt_max_apart(None)
@@ -177,7 +190,7 @@ class CursorImagerGUI:
             ("Portable Network Graphics", "*.png"),
             ("Joint Photographics Expert Group", "*.jpg")
         ]
-        filename = filedialog.askopenfilename(title="Open Image", filetypes=filetypes)
+        filename = ctk.filedialog.askopenfilename(title="Open Image", filetypes=filetypes)
         if filename:
             self.image_path = filename
             self.use_clipboard_image = False
@@ -189,12 +202,13 @@ class CursorImagerGUI:
             global img # this is critical so the garbage collector does not dispose of the image
                        # otherwise the displayed "image" will just be blank
             img = self.drawing_machine.preview(1)
-            img = ImageTk.PhotoImage(Image.fromarray(img).convert('RGB'))
+            pil_img = Image.fromarray(img).convert('RGB')
+            img = ctk.CTkImage(pil_img, pil_img, size=pil_img.size)
             self.imgDisplay.configure(image=img)
             self.lblPointCount.configure(text=f"Point count: {sum([len(x) for x in self.drawing_machine.contours])}")
     
     def txt_hotkey_type(self, e):
-        self.txtSetHotkey.delete(0, END)
+        self.txtSetHotkey.delete(0, ctk.END)
         print(f"Bind Hotkey: {e.char}")
         
         new_hotkey = e.char
@@ -213,13 +227,19 @@ class CursorImagerGUI:
         self.drawing_machine.reset()
 
     def scale_speed_slide(self, e):
-        self.params.sleep_between_action = 1.0 / self.scaleSpeedValue.get() * 0.02;
+        v = self.scaleSpeedValue.get()
+        self.params.sleep_between_action = 1.0 / v * 0.02
+        self.lblSpeedInfo.configure(text="Drawing Speed: %i" % v)
 
     def scale_scale_slide(self, e):
-        self.params.scale = int(self.scaleScaleValue.get())
+        v = self.scaleScaleValue.get()
+        self.params.scale = int(v)
+        self.lblScaleInfo.configure(text="Drawing Scale: %i" % v)
     
     def scale_decimation_slide(self, e):
-        self.params.decimate = int(self.scaleDecimationValue.get())
+        v = self.scaleDecimationValue.get()
+        self.params.decimate = int(v)
+        self.lblDecimationInfo.configure(text="Point Decimation: %i" % v)
 
     def cbDoEdge_press(self):
         self.params.do_edge = self.cbDoEdgeValue.get()
@@ -258,8 +278,8 @@ class CursorImagerGUI:
             self.drawing_machine.active_change()
 
     def main(self):
-        self.window.protocol("WM_DELETE_WINDOW", self.on_close)
-        self.window.mainloop()
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
+        self.root.mainloop()
     
     def on_close(self):
         print("Closing.")
@@ -271,4 +291,6 @@ class CursorImagerGUI:
         os._exit(0) # drawing thread(s) does/do not terminate by itself
 
 if __name__=="__main__":
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
     CursorImagerGUI().main()
